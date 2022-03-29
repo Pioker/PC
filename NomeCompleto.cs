6c241d244f5b312d4ed6ef85643c39d37f7efc5d
Pioker <gipioker@hotmail.com>
2022-03-28 22:17:37 -0300

    first commit

---
+++ NomeCompleto.cs
@@ -0,0 +1,10 @@
+string nome, sobrenome;
+
+Console.Write("Digite seu primeiro nome:");
+nome = Console.ReadLine();
+
+Console.Write("Digite seu sobrenome....:");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"Nome completo...: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()},{nome}");
